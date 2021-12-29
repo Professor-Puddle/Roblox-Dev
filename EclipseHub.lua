523e52f5e6602c8e84b19149a8dfdf47f7afa1f1
Professor Puddle <78994826+Professor-Puddle@users.noreply.github.com>
2021-12-29 16:44:50 +0530

    Update EclipseHub.lua

--- EclipseHub.lua
+++ EclipseHub.lua
@@ -9,12 +9,12 @@
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local EclipseHub = Instance.new("TextButton")
-local X = Instance.new("TextButton")
-local TextLabel = Instance.new("TextLabel")
+local Close = Instance.new("TextButton")
+local Info = Instance.new("TextLabel")
 
 --Properties:
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.Parent = game.CoreGui
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Frame.Parent = ScreenGui
@@ -22,41 +22,41 @@ Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.Position = UDim2.new(0.20923306, 0, 0.592317224, 0)
 Frame.Size = UDim2.new(0, 178, 0, 175)
 
-EclipseHub.Name = "Eclipse Hub"
+EclipseHub.Name = "EclipseHub"
 EclipseHub.Parent = Frame
 EclipseHub.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-EclipseHub.Position = UDim2.new(0.220055714, 0, 0.172682941, 0)
-EclipseHub.Size = UDim2.new(0, 200, 0, 50)
-EclipseHub.Visible = false
+EclipseHub.Position = UDim2.new(0.163875967, 0, 0.709825993, 0)
+EclipseHub.Size = UDim2.new(0, 117, 0, 31)
 EclipseHub.Font = Enum.Font.SourceSans
-EclipseHub.Text = "Execute Eclipse Hub"
+EclipseHub.Text = "Execute Manually"
 EclipseHub.TextColor3 = Color3.fromRGB(0, 0, 0)
 EclipseHub.TextSize = 14.000
 
-X.Name = "X"
-X.Parent = Frame
-X.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-X.Position = UDim2.new(0.813448668, 0, -0.00134727359, 0)
-X.Size = UDim2.new(0, 33, 0, 30)
-X.Font = Enum.Font.SourceSans
-X.Text = "X"
-X.TextColor3 = Color3.fromRGB(0, 0, 0)
-X.TextSize = 14.000
+Close.Name = "Close"
+Close.Parent = Frame
+Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Close.Position = UDim2.new(0.813448668, 0, -0.00134727359, 0)
+Close.Size = UDim2.new(0, 33, 0, 30)
+Close.Font = Enum.Font.SourceSans
+Close.Text = "X"
+Close.TextColor3 = Color3.fromRGB(0, 0, 0)
+Close.TextSize = 14.000
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.0693561733, 0, 0.202044144, 0)
-TextLabel.Size = UDim2.new(0, 152, 0, 103)
-TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "Eclipse Hub will execute itself in 5 seconds, and this GUI will close. Eclipse Hub was made by £thanoj1#3304"
-TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.TextSize = 14.000
-TextLabel.TextWrapped = true
+Info.Name = "Info"
+Info.Parent = Frame
+Info.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Info.BorderSizePixel = 0
+Info.Position = UDim2.new(0.0693561658, 0, 0.230615586, 0)
+Info.Size = UDim2.new(0, 152, 0, 72)
+Info.Font = Enum.Font.SourceSans
+Info.Text = "Eclipse Hub will execute itself in 5 seconds, and this GUI will close. Eclipse Hub was made by £thanoj1#3304"
+Info.TextColor3 = Color3.fromRGB(0, 0, 0)
+Info.TextSize = 14.000
+Info.TextWrapped = true
 
 -- Scripts:
 
-local function FRAWH_fake_script() -- EclipseHub.LocalScript 
+local function CMXUN_fake_script() -- EclipseHub.LocalScript 
 	local script = Instance.new('LocalScript', EclipseHub)
 
 	script.Parent.MouseButton1Click:Connect(function()
@@ -65,17 +65,17 @@ local function FRAWH_fake_script() -- EclipseHub.LocalScript
 		script.Parent.Parent.Parent:Destroy()
 	end)
 end
-coroutine.wrap(FRAWH_fake_script)()
-local function IRUH_fake_script() -- X.LocalScript 
-	local script = Instance.new('LocalScript', X)
+coroutine.wrap(CMXUN_fake_script)()
+local function ZVIXJA_fake_script() -- Close.LocalScript 
+	local script = Instance.new('LocalScript', Close)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Parent.Parent:Destroy()
 	end)
 		
 end
-coroutine.wrap(IRUH_fake_script)()
-local function FKYZZ_fake_script() -- Frame.LocalScript 
+coroutine.wrap(ZVIXJA_fake_script)()
+local function KTXSK_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	script.Parent.Active = true
@@ -85,4 +85,4 @@ local function FKYZZ_fake_script() -- Frame.LocalScript
 	local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth"c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
 	script.Parent:Destroy()
 end
-coroutine.wrap(FKYZZ_fake_script)()
+coroutine.wrap(KTXSK_fake_script)()
